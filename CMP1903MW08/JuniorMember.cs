862cf42151b6027dfa52278004c9daa52bb907ec
billyedwards3017 <43846656+billyedwards3017@users.noreply.github.com>
2020-03-24 16:25:31 +0000

    Added all the code and shizz

---
+++ CMP1903MW08/JuniorMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903MW08
+{
+    class JuniorMember
+    {
+        //Golf Club membership for under 18s
+    }
+}
---
+++ CMP1903MW08/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903MW08
+{
+    class Member
+    {
+        //'Normal' member of Golf Club
+    }
+}
